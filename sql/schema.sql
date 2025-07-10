b8cca9cdc5262b0184e74062a40f3e1bdf9974dd
Mahdi732 <ey449751@gmail.com>
2025-07-10 10:24:22 +0100

    add the sql code to create table users and role

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+-- role table 
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- inserte roles 
+INSERT INTO roles (nom) VALUES
+('Gestionnaire'),
+('Commercial'),
+('Technicien'),
+('Client');
+
+-- create users table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    role_id INT NOT NULL,
+    date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+)
